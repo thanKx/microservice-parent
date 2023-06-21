8bb9de56e4120d46c207e05842266e2f8ef66724
xieq <727170890@qq.com>
2023-06-21 14:04:52 +0800

    feign服务调用

---
+++ api/system-api/src/main/java/org/xieq/system/api/TestFeignClient.java
@@ -0,0 +1,15 @@
+package org.xieq.system.api;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+
+/**
+ * @author xieq
+ * @date 2023/6/21 08:48
+ */
+@FeignClient("system")
+public interface TestFeignClient {
+
+    @GetMapping("hello")
+    String hello();
+}
